f8327a670af39917b881c86c9b409f2a74e94413
Jaechan Lee <dlwocks31@gmail.com>
2024-06-17 23:45:04 +0900

    data before submission

--- esm_inference.py
+++ esm_inference.py
@@ -9,6 +9,27 @@ from lib.utils import dict_tensor_to_num
 from atpbind_main import ALL_PARAMS
 
 
+params = [
+    {
+        'model': 'esm-t33-gearnet',
+        'weights': [[f'atpbind3d_lm-gearnet_{i}.pt'] for i in range(5)],
+    },
+    {
+        'model': 'esm-t33-gearnet-adaboost-r10',
+        'weights': [
+            [f'atpbind3d_esm-t33-gearnet-adaboost-r10_{fold}_{i}.pt' for i in range(10)]
+            for fold in range(5)
+        ],
+    },
+    {
+        'model': 'esm-t33-gearnet-resiboost-r10',
+        'weights': [
+            [f'atpbind3d_esm-t33-gearnet-resiboost-r10_{fold}_{i}.pt' for i in range(10)]
+            for fold in range(5)
+        ],
+    }
+]
+
 def esm_inference(model_key, weights, weight_base, threshold, gpu):
     model_config = ALL_PARAMS[model_key]
     pipeline = Pipeline(
@@ -40,45 +61,37 @@ def esm_inference(model_key, weights, weight_base, threshold, gpu):
     
     return result
 
-params = [
-    {
-        'label': 'esm-t33-gearnet',
-        'weights': [[f'atpbind3d_lm-gearnet_{i}.pt'] for i in range(5)],
-        'thresholds': [-3.0, -2.6, -3.0, -2.2, -0.3]
-    },
-    {
-        'label': 'esm-t33-gearnet-adaboost-r10',
-        'weights': [
-            [f'atpbind3d_esm-t33-gearnet-adaboost-r10_{fold}_{i}.pt' for i in range(10)] 
-            for fold in range(5)
-        ],
-        'thresholds':  [-1.1, -1.4, -1.7, -0.9, -1.2],
-    },
-    {
-        'label': 'esm-t33-gearnet-resiboost-r10',
-        'weights': [
-            [f'atpbind3d_esm-t33-gearnet-resiboost-r10_{fold}_{i}.pt' for i in range(10)] 
-            for fold in range(5)
-        ],
-        'thresholds': [-0.1, -0.4, -1.4, -1.3, -0.4]
-    }
-]
 
 def main(gpu):
     results = []
     for param in params:
-        print(f'Running {param["label"]}')
-        for weights, threshold in zip(param['weights'], param['thresholds']):
+        print(f'Running {param["model"]}')
+        for fold, weights in enumerate(param['weights']):
+            # Searching for the best threshold computed according to the validation set
+            # which is stored in the below file in training process
+            stat_df = pd.read_csv(f'result/atpbind3d_stats.csv')
+            try:
+                threshold = stat_df[(stat_df['model_key'] == param['model']) & (
+                    stat_df['valid_fold'] == fold)].iloc[0]['best_threshold']
+                print(f'Fold {fold}: found best threshold {threshold}')
+            except IndexError:
+                threshold = -1.0
+                print(f'Fold {fold}: best threshold not found, using default {threshold}')
+                
+            # do inference
             result = esm_inference(
                 model_key='esm-t33-gearnet', 
                 weights=weights, 
                 weight_base='weight', 
                 threshold=threshold, 
-                gpu=0
+                gpu=gpu,
             )
-            results.append({'model': param['label'], 'threshold': threshold, **result})
-        df = pd.DataFrame(results)
-        df.to_csv('result/atpbind3d_esm_stats.csv', index=False)
+            print(f'Fold {fold}: result {result}')
+            results.append({'model': param['model'], 'fold': fold, 'threshold': threshold, **result})
+            
+            # save results
+            df = pd.DataFrame(results)
+            df.to_csv('result/atpbind3d_esm_stats.csv', index=False)
     
     
     
